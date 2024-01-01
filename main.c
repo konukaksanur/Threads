87f632d4de6c5b8df0b58b00bd5baa35d8937768
Duru Baştunalı <durubastunali@gmail.com>
2024-01-01 23:20:23 +0300

    ödevin %75'ini yaptım

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
-long long int a, b, x;
+long long int a, b;
 int c, d;
 double global_sqrt_sum = 0;
 
+double method1();
+double method2();
+double method3();
+
 int main() {
+    switch(d) {
+        case 1: method1(); break;
+        case 2: method2(); break;
+        case 3: method3(); break;
+        default: fprintf(stderr, "Invalid d.");
+    }
     return 0;
+}
+
+double method1() {
+}
+
+double method2() {
+}
+
+double method3() {
 }
